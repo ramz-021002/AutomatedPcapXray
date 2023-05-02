bfe822c3d1302667bead07ec0669d00232d73336
P Rama Ramana Sharma <95123123+ramz-021002@users.noreply.github.com>
2023-05-02 11:10:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+# Main File - Driver for the Application PcapXray
+
+# Import Libraries
+import os   #-- default lib - packed with python
+import sys  #-- default lib
+import datetime  #-- default lib
+import time
+if sys.platform == 'darwin':
+    import matplotlib
+    matplotlib.use('TkAgg')
+
+interactive_graph_support = False
+try:
+    from cefpython3 import cefpython as cef
+    interactive_graph_support = True
+except:
+    print("Interactive graph in app wont work as python version/platform is not supported (will launch in default browser)")
+    pass
+
+if sys.path[0]:
+    sys.path.insert(0,sys.path[0]+'/Module/')
+else:
+    sys.path.insert(0, 'Module/')
+
+import user_interface
+
+# Import 3rd party Libraries -- Needed to be installed using pip
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+def main():
+    while True:
+        user_interface.pcapXrayCLI()
+        #main.clear()
+    time.sleep(1000)
+main()
+
+    
